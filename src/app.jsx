13c92328f08d682c566ed3f5cc94ae4b28e514c9
laridev@gmail.com <laridev@gmail.com>
2019-09-08 23:24:48 +0100

    finish off search section

--- src/app.jsx
+++ src/app.jsx
@@ -4,12 +4,32 @@ import FormControl from "@material-ui/core/FormControl";
 import TextField from "@material-ui/core/TextField";
 import { makeStyles } from "@material-ui/core/styles";
 import Fab from "@material-ui/core/Fab";
-import AddIcon from "@material-ui/icons/Add";
+import SearchIcon from "@material-ui/icons/Search";
+import Grid from "@material-ui/core/Grid";
+import InputBase from '@material-ui/core/InputBase';
+import IconButton from '@material-ui/core/IconButton';
 export default function App() {
   const useStyles = makeStyles(theme => ({
     container: {
       display: "flex",
       flexWrap: "wrap"
+    },
+    root: {
+      // padding: '2px 4px',
+      padding : '10% 3%',
+      display: 'flex',
+      alignItems: 'center',
+      width: 400,
+    },
+    input: {
+      marginLeft: theme.spacing(1),
+      flex: 1,
+      // border : '1px solid rgba(209,209,209,1)',
+      padding: "1% 5%",
+      borderRadius: "6px",
+      direction:'rtl',
+      boxShadow: '-4px 10px 21px -1px rgba(209,209,209,1)'
+            
     },
     textField: {
       marginLeft: theme.spacing(1),
@@ -20,6 +40,11 @@ export default function App() {
     },
     menu: {
       width: 200
+    },
+    iconButton : {
+      // border : '1px solid rgba(209,209,209,1)',
+      marginLeft :'2%',
+      boxShadow: '-4px 10px 21px -1px rgba(209,209,209,1)'
     }
   }));
   const classes = useStyles();
@@ -28,20 +53,15 @@ export default function App() {
       <div className="playerWrapper">
         <div className="left-part">
           <div className="search-section">
-            <form>
-              <FormControl>
-                <TextField
-                  id="outlined-search"
-                  label="Search field"
-                  type="search"
-                  className={classes.textField}
-                  margin="normal"
-                  variant="outlined"
-                />
-                <Fab color="primary" aria-label="add" className={classes.fab}>
-                  <AddIcon />
-                </Fab>
-              </FormControl>
+            <form className={classes.root}>
+            <IconButton className={classes.iconButton} aria-label="search">
+                <SearchIcon />
+              </IconButton>
+              <InputBase
+                className={classes.input}
+                placeholder="ابحث عن قارئ"
+                inputProps={{ "aria-label": "ابحث عن قارئ" }}
+              />
             </form>
           </div>
           <div className="recetors-section"></div>
