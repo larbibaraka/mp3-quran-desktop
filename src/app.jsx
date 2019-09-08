0b8f10e3b06b1a54614ef5a38bf02f2bdf9b0244
laridev@gmail.com <laridev@gmail.com>
2019-09-08 22:11:19 +0100

    :clap: updating npm dependencies :clap:

--- src/app.jsx
+++ src/app.jsx
@@ -1,11 +1,15 @@
-import React from "react";
+/* eslint-disable react/jsx-indent */
+import React from 'react';
+import Button from '@material-ui/core/Button';
 
 export default class App extends React.Component {
   render() {
     return (
-      <div>
-        <h2>Baraka Larbi !</h2>
-      </div>
+    <div>
+      <Button variant="contained" color="primary">
+        Hello World
+      </Button>
+    </div>
     );
   }
 }
